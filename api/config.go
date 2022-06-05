b55ba53340c6d7282c424f2be1ec6ba65bfe457d
jujusti <147706172+jujusti@users.noreply.github.com>
2022-06-05 07:38:56 +0000

    Version bump to 0.1.11

--- api/config.go
+++ api/config.go
@@ -27,4 +27,6 @@
  * encouraged to load and test the software's suitability as regards their
  * requirements in conditions enabling the security of their systems and/or
  * data to be ensured and,  more generally, to use and operate it in the
- * same conditions a
+ * same conditions as regards security.
+ *
+ * The fact that you are presently 

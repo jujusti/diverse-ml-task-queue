7199a279d2b2191fee6cfc527e14d19a8033c42b
jujusti <147706172+jujusti@users.noreply.github.com>
2022-06-05 22:40:46 +0000

    Bump version to 0.8.0

--- api/config.go
+++ api/config.go
@@ -30,4 +30,7 @@
  * same conditions as regards security.
  *
  * The fact that you are presently reading this means that you have had
- * knowledge of
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
+pac

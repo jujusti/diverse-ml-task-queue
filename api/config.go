bfa4e8b71e09067c6802fe0e7448ee14d2af929b
jujusti <147706172+jujusti@users.noreply.github.com>
2022-07-01 11:02:06 +0000

    Update hyde.css

--- api/config.go
+++ api/config.go
@@ -75,4 +75,5 @@ func (c *ProducerConfig) Unlock() {
 
 // NewProducerConfig computes the configuration object. Note that a pointer is returned not to avoid
 // copy but rather to allow the configuration to be dynamically changed.  If this isn't possible
-// with a flags or env. variables, we may later make it possible to ge
+// with a flags or env. variables, we may later make it possible to get the config from a K/V store
+// such as etcd or consul to 

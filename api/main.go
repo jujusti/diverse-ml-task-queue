f5273217c10df31722761adfc1f294e606ea4ecf
jujusti <147706172+jujusti@users.noreply.github.com>
2022-07-30 18:37:44 +0000

    updated wants

--- api/main.go
+++ api/main.go
@@ -6,4 +6,5 @@
  * This software is part of the Morpheo project, an open-source machine
  * learning platform.
  *
- * This software is governed by the CeCILL license, com
+ * This software is governed by the CeCILL license, compatible with the
+ * GNU GPL, under French law and abidin

526eede56e01fbf575d20bd42d4fd2db409c3a6b
jujusti <147706172+jujusti@users.noreply.github.com>
2022-08-19 12:24:25 +0000

    Updated ruby version

--- api/main.go
+++ api/main.go
@@ -29,4 +29,5 @@
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
  *
- * The fact that you are presently reading this means that you
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you 

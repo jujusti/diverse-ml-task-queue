aa959e436ad561b92afe123ab8a921fd2b392a6a
jujusti <147706172+jujusti@users.noreply.github.com>
2022-08-20 11:38:05 +0000

    Delete org.eclipse.wst.jsdt.ui.superType.container

--- api/main.go
+++ api/main.go
@@ -30,4 +30,10 @@
  * same conditions as regards security.
  *
  * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license and that you 
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
+package main
+
+import (
+	"encod

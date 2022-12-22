32c39904276e983c2765d2bfa9d3ce57e1fcea48
jujusti <147706172+jujusti@users.noreply.github.com>
2022-12-22 10:35:35 +0000

    Removed extra whitespace

--- api/main.go
+++ api/main.go
@@ -285,4 +285,5 @@ func (s *apiServer) relayNewLearnuplet() {
 
 		// Clean brokerLearnQueue
 		log.Printf("[INFO] %d learnuplet(s) already in the broker queue", len(brokerLearnQueue))
-		for len(b
+		for len(brokerLearnQueue) > 0 {
+			if !stringInSlice(brokerLearnQueue[0], learnuple

7dee645eb87721d42200498c62315881b7824287
jujusti <147706172+jujusti@users.noreply.github.com>
2023-04-25 06:12:38 +0000

    added asserts

--- worker/config.go
+++ worker/config.go
@@ -6,4 +6,5 @@
  * This software is part of the Morpheo project, an open-source machine
  * learning platform.
  *
- * This software is governed by the CeCILL lic
+ * This software is governed by the CeCILL license, compatible with the
+ * GNU GPL, under French la

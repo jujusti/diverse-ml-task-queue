8d2338a420fb1d19338cb96a00bb5240a19382ac
jujusti <147706172+jujusti@users.noreply.github.com>
2023-05-29 06:42:52 +0000

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- worker/config.go
+++ worker/config.go
@@ -30,4 +30,11 @@
  * same conditions as regards security.
  *
  * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license and that you accep
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
+package main
+
+import (
+	"flag"
+	"time"

ec0cd4ed0315fc2bbcfdb4421190bbd1c9895a25
jujusti <147706172+jujusti@users.noreply.github.com>
2023-07-13 11:22:25 +0000

    Fix missing semicolon.

--- worker/config.go
+++ worker/config.go
@@ -92,4 +92,4 @@ func NewConsumerConfig() (conf *ConsumerConfig) {
 
 	// CLI Flags
 	flag.Var(&nsqlookupdURLs, "nsqlookupd-urls", "URL(s) of NSQLookupd instances to connect to")
-	flag.StringVar(&nsqdURL, "h
+	flag.StringVar(&nsqdURL, "http-address", "nsqd:4151", "URL of NSQd instance to

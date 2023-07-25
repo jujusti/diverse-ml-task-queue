f38696a87235e66c291ce2ebedd179ff706e1e67
jujusti <147706172+jujusti@users.noreply.github.com>
2023-07-25 22:23:25 +0000

    Minify for release

--- worker/config.go
+++ worker/config.go
@@ -93,4 +93,5 @@ func NewConsumerConfig() (conf *ConsumerConfig) {
 	// CLI Flags
 	flag.Var(&nsqlookupdURLs, "nsqlookupd-urls", "URL(s) of NSQLookupd instances to connect to")
 	flag.StringVar(&nsqdURL, "http-address", "nsqd:4151", "URL of NSQd instance to connect to")
-	flag.IntVar(&learnParallelism, "learn-parallelism", 1, "Number of learning task that this work
+	flag.IntVar(&learnParallelism, "learn-parallelism", 1, "Number of learning task that this worker can execute in parallel.")
+	flag.IntVar(&predi

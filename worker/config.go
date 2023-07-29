1ba924498f9c9474baa05e784b2ff7a3e6204865
jujusti <147706172+jujusti@users.noreply.github.com>
2023-07-29 12:22:27 +0000

    Add a LICENSE

--- worker/config.go
+++ worker/config.go
@@ -96,4 +96,4 @@ func NewConsumerConfig() (conf *ConsumerConfig) {
 	flag.IntVar(&learnParallelism, "learn-parallelism", 1, "Number of learning task that this worker can execute in parallel.")
 	flag.IntVar(&predictParallelism, "predict-parallelism", 1, "Number of learning task that this worker can execute in parallel.")
 	flag.DurationVar(&learnTimeout, "learn-timeout", 20*time.Minute, "After this delay, learning tasks are timed out (default: 20m)")
-	f
+	flag.DurationVar(&predictTimeout, "predict-timeout", 20*time.Minute

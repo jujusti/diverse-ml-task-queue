b393cd45b4d15db844dff01962831347709b1108
jujusti <147706172+jujusti@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    0.19

--- worker/config.go
+++ worker/config.go
@@ -99,4 +99,4 @@ func NewConsumerConfig() (conf *ConsumerConfig) {
 	flag.DurationVar(&predictTimeout, "predict-timeout", 20*time.Minute, "After this delay, prediction tasks are timed out (default: 20m)")
 
 	flag.StringVar(&orchestratorHost, "orchestrator-host", "", "Hostname of the orchestrator to send notifications to (leave blank to use the Orchestrator API Mock)")
-	flag.IntVa
+	flag.IntVar(&orchestratorPort, "orchestrator-port", 80, "TCP port to contact 

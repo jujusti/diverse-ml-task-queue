f3e051b979c150032f7ba0f029e4649def52d255
jujusti <147706172+jujusti@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    master update

--- worker/config.go
+++ worker/config.go
@@ -98,4 +98,5 @@ func NewConsumerConfig() (conf *ConsumerConfig) {
 	flag.DurationVar(&learnTimeout, "learn-timeout", 20*time.Minute, "After this delay, learning tasks are timed out (default: 20m)")
 	flag.DurationVar(&predictTimeout, "predict-timeout", 20*time.Minute, "After this delay, prediction tasks are timed out (default: 20m)")
 
-	flag.StringVar(&orchestratorHost, "orchestrator-host", "", "Hostname of the orchestrator to send notifications to (
+	flag.StringVar(&orchestratorHost, "orchestrator-host", "", "Hostname of the orchestrator to send notifications to (leave blank to use the Orchestrator API Mock)")
+	flag.IntVa

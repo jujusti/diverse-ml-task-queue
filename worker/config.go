bfa259e4653b01d7ece0f7d2a5bf427c91aa4c35
jujusti <147706172+jujusti@users.noreply.github.com>
2023-09-17 22:22:28 +0000

    Add linter

--- worker/config.go
+++ worker/config.go
@@ -127,4 +127,5 @@ func NewConsumerConfig() (conf *ConsumerConfig) {
 		// Other compute services
 		OrchestratorHost:     orchestratorHost,
 		OrchestratorPort:     orchestratorPort,
-		Orchestrator
+		OrchestratorUser:     orchestratorUser,
+		OrchestratorPassword

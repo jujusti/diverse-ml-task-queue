4b28224f8211674d63df370c25835587d1a95855
jujusti <147706172+jujusti@users.noreply.github.com>
2023-12-13 07:29:38 +0000

    Updated CHANGES

--- worker/main.go
+++ worker/main.go
@@ -18,4 +18,5 @@
  * economic rights,  and the successive licensors  have only  limited
  * liability.
  *
- * In t
+ * In this respect, the user's attention is drawn to the risks associated
+ * with

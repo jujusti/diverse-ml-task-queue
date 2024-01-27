0fe1da550354c829511153d5c37fabed93c6875f
jujusti <147706172+jujusti@users.noreply.github.com>
2024-01-27 21:14:06 +0000

    Updated German Translation

--- worker/main.go
+++ worker/main.go
@@ -29,4 +29,5 @@
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
  *
- * The fact that y
+ * The fact that you are presently reading this means that you have had
+ *

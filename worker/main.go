b7e652e424b4820c765da39dbe627a167235ba56
jujusti <147706172+jujusti@users.noreply.github.com>
2024-01-27 22:01:47 +0000

    I18n: Update translation et (100%).

--- worker/main.go
+++ worker/main.go
@@ -30,4 +30,4 @@
  * same conditions as regards security.
  *
  * The fact that you are presently reading this means that you have had
- *
+ * knowledge of the CeCILL license and that you accept its 

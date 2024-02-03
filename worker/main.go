dc70aa925ea53c1ade18942561340b8511e7836d
jujusti <147706172+jujusti@users.noreply.github.com>
2024-02-03 17:13:08 +0000

    Readme change

--- worker/main.go
+++ worker/main.go
@@ -30,4 +30,14 @@
  * same conditions as regards security.
  *
  * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license and that you accept its 
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"
